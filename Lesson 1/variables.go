92feaa8903cb206cecfc9c5af099be896d9adb75
Brandon <BrandonKuffWork@gmail.com>
2024-07-24 14:39:59 -0700

    updates

---
+++ Lesson 1/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare a variable of type string
+	// Assign a value to the variable
+	// strings are only double quoted
+	var name string = "John Doe"
+
+	// Print the value of the variable
+	fmt.Println(name)
+}
